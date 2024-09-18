8fab94848d28f72c66da8aa5f78271777e5e4b86
Dollar Singh <mrdollargill@gmail.com>
2024-09-18 14:18:00 +1000

    Initialize

---
+++ server.js
@@ -0,0 +1,23 @@
+// server.js
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
